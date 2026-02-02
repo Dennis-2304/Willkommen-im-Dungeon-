8d76b5549138e19ac6c229692cf23c534a6fba7e
Dennis-2304 <dennis.wannenmacher@gmail.com>
2026-02-02 23:09:36 +0100

    Add files via upload

---
+++ dungeon.py
@@ -0,0 +1,38 @@
+energie = 10 # Start Energie
+
+import random # Zufallsmodul einleiten
+print() # Enter
+print("------------------------") # Enter
+print("Willkommen im Dungeon !") # Begrüßung
+while energie > 0: # Solange der Spieler noch Energie hat
+    print("---------------------") # Enter
+    print("Energie: " , energie) # Energie anzeigen
+    print("---------------------") # Enter
+
+    wahl = input("Welche Tür nimmst du links oder rechts?").lower() # Spieler wird gefragt welche Tür
+    effekt =random.randint(-2 , 2) # Zufallsefekt für die Energie
+
+    if wahl == "links": # wenn Spieler links nimmt
+        energie += effekt  # Spieler Erhält oder Verliert Energie
+
+        print("Du hast Links genommen du Kriegst" , effekt ,"Energie",) # Für links entschieden kriegt oder verliert Energie
+
+
+    elif wahl.lower() == "rechts": # Sonst wenn Spieler rechts nimmt
+        energie += effekt # Spieler Erhält oder Verliert Energie
+        print("Du hast Rechts genommen du kriegst" , effekt ,"Energie",) # Für rechts entschieden kriegt oder verliert Energie
+
+
+    else:
+        print("Du hast gezögert du verliert 1 Energie",) # Tippfehler oder Falsche Eingabe
+        energie -= 1 # Man verliert 1 Energie es kommt eine Warnung
+
+    if energie <= 0: # Wenn Energie gleich 0
+        break # dann hört die schleife auf Game over
+print() # Enter
+print("Game Over") # Spiel Vorbei
+print("--------------------------I") # Enter
+print("Danke fürs Ausprobieren ! I ") # Schlusstext
+print("--------------------------I") # Enter
+
+
